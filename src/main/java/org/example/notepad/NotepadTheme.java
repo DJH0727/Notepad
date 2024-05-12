e9d5c18ff66c0458a978b7f4cbee4cb1ccfb8a57
DJH0727 <2724948893@qq.com>
2024-05-12 16:30:22 +0800

    2024/5/12 基本完善

--- src/main/java/org/example/notepad/NotepadTheme.java
+++ src/main/java/org/example/notepad/NotepadTheme.java
@@ -23,15 +23,22 @@ public class NotepadTheme {
     static String savedFont="System";
 
     static Color savedColor = Color.BLACK;
+    static FontWeight savedFontWeight = FontWeight.NORMAL;
+
+    static FontPosture savedFontPosture = FontPosture.REGULAR;
 
     public static void initTheme(TextArea textArea) {
         textArea.setStyle("-fx-text-fill: " + toHexString(savedColor) + ";");
-        textArea.setFont(Font.font(savedFont, FontWeight.NORMAL, FontPosture.REGULAR, savedSize));
+        textArea.setFont(Font.font(savedFont, savedFontWeight, savedFontPosture, savedSize));
     }
 
 
     boolean isChange = false;
     TextArea textArea;
+
+
+
+
     //演示文本
     Text demoText = new Text("你好，世界！\nHello, World!\n1234567890");
     public NotepadTheme(TextArea textArea) {
@@ -40,8 +47,12 @@ public class NotepadTheme {
         currentSize = savedSize;
         currentColor = savedColor;
         currentFont = savedFont;
-        demoText.setFont(Font.font(savedFont, FontWeight.NORMAL, FontPosture.REGULAR, savedSize));
+        currentFontWeight = savedFontWeight;
+        currentFontPosture = savedFontPosture;
+        demoText.setFont(Font.font(savedFont, savedFontWeight, savedFontPosture, savedSize));
         demoText.setFill(savedColor);
+        boldCheck.setSelected(savedFontWeight == FontWeight.BOLD);
+        italicCheck.setSelected(savedFontPosture == FontPosture.ITALIC);
 
     }
 
@@ -53,7 +64,11 @@ public class NotepadTheme {
     String[] sizes= {"8","9","10","11","12","13","14","15","16","18","20","22","24","26","28","36","48","72"};
     Color currentColor;
     String currentFont;
+    CheckBox boldCheck = new CheckBox("粗体");
+    CheckBox italicCheck = new CheckBox("斜体");
 
+    FontWeight currentFontWeight;
+    FontPosture currentFontPosture;
 
     public void showChangeStage(Stage stage) {
 
@@ -91,7 +106,9 @@ public class NotepadTheme {
         gridPane.add(fontComboBox, 1, 1);
         gridPane.add(colorLabel, 0, 2);
         gridPane.add(colorPicker, 1, 2);
-        gridPane.add(new Label("演示文本："), 0, 3);
+        gridPane.add(boldCheck, 0, 3);
+        gridPane.add(italicCheck, 1, 3);
+        gridPane.add(new Label("演示文本："), 0, 4);
 
         VBox demoBox = new VBox(demoText);
         demoBox.setAlignment(Pos.CENTER);
@@ -99,8 +116,34 @@ public class NotepadTheme {
 
         //gridPane.add(demoText, 0, 3);
         //gridPane.setAlignment(Pos.CENTER);
+        boldCheck.selectedProperty().addListener((observable, oldValue, newValue) -> {
 
+            if (newValue) {
+                currentFontWeight = FontWeight.BOLD;
+                savedFontWeight = currentFontWeight;
+                demoText.setFont(Font.font(currentFont, currentFontWeight, currentFontPosture, currentSize));
+                textArea.setFont(Font.font(currentFont, currentFontWeight, currentFontPosture, currentSize));
+            } else {
+                currentFontWeight = FontWeight.NORMAL;
+                savedFontWeight = currentFontWeight;
+                demoText.setFont(Font.font(currentFont, currentFontWeight, currentFontPosture, currentSize));
+                textArea.setFont(Font.font(currentFont, currentFontWeight, currentFontPosture, currentSize));
+            }
+        });
+        italicCheck.selectedProperty().addListener((observable, oldValue, newValue) -> {
 
+            if (newValue) {
+                currentFontPosture = FontPosture.ITALIC;
+                savedFontPosture = currentFontPosture;
+                demoText.setFont(Font.font(currentFont, currentFontWeight, currentFontPosture, currentSize));
+                textArea.setFont(Font.font(currentFont, currentFontWeight, currentFontPosture, currentSize));
+            } else {
+                currentFontPosture = FontPosture.REGULAR;
+                savedFontPosture = currentFontPosture;
+                demoText.setFont(Font.font(currentFont, currentFontWeight, currentFontPosture, currentSize));
+                textArea.setFont(Font.font(currentFont, currentFontWeight, currentFontPosture, currentSize));
+            }
+        });
 
 
         sizeComboBox.getEditor().textProperty().addListener(new ChangeListener<String>() {
@@ -114,8 +157,8 @@ public class NotepadTheme {
                 else {
                     currentSize = Integer.parseInt(newValue);
                     savedSize = currentSize;
-                    demoText.setFont(Font.font(savedFont, FontWeight.NORMAL, FontPosture.REGULAR, savedSize));
-                    textArea.setFont(Font.font(currentFont, FontWeight.NORMAL, FontPosture.REGULAR, currentSize));
+                    demoText.setFont(Font.font(currentFont, currentFontWeight, currentFontPosture, currentSize));
+                    textArea.setFont(Font.font(currentFont, currentFontWeight, currentFontPosture, currentSize));
                 }
             }
         });
@@ -124,8 +167,8 @@ public class NotepadTheme {
 
             currentFont = newValue;
             savedFont = currentFont;
-            demoText.setFont(Font.font(savedFont, FontWeight.NORMAL, FontPosture.REGULAR, savedSize));
-            textArea.setFont(Font.font(newValue, FontWeight.BOLD, FontPosture.REGULAR, currentSize));
+            demoText.setFont(Font.font(currentFont, currentFontWeight, currentFontPosture, currentSize));
+            textArea.setFont(Font.font(currentFont, currentFontWeight, currentFontPosture, currentSize));
         });
 
         colorPicker.setOnAction(e -> changeColor());
@@ -153,7 +196,7 @@ public class NotepadTheme {
 
          currentColor = colorPicker.getValue();
          savedColor = currentColor;
-        demoText.setFill(currentColor);
+         demoText.setFill(currentColor);
          textArea.setStyle("-fx-text-fill: " + toHexString(currentColor) + ";");
         // 更改字体颜色
         //textArea.setStyle("-fx-text-fill: red;");
@@ -164,8 +207,6 @@ public class NotepadTheme {
                 (int) (color.getGreen() * 255),
                 (int) (color.getBlue() * 255));
     }
-
-    //允许用户更改文本的字体样式、大小和颜色
 
 
 
